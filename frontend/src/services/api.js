41966034ebedd62d306baf8c3068a1503a2617be
neggaaaa <kangeldi2005@mail.ru>
2025-05-25 03:02:24 +0500

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const uploadFile = async (file) => {
   const formData = new FormData();
